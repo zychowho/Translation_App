27e877823c72acc17af2c2dafcd1fe78ee84edf0
zychowho <paulvincentbua6@gmail.com>
2025-02-10 22:26:16 +0800

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import  'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:translation_app/login/login.dart';
